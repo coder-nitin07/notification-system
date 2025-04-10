356cdac37511461a216036378049c20a04c350b0
coder-nitin07 <nitinthakur87504@gmail.com>
2025-04-10 15:43:37 +0530

    Update sendMail.js

--- services/sendMail.js
+++ services/sendMail.js
@@ -3,9 +3,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// MailGun domain
 const DOMAIN = process.env.MAILGUN_DOMAIN;
+
+// mailGun API Key
 const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
 
+// send the Mail Information log
 const sendEmail = (to, subject, text, html) => {
   const data = {
     from: 'postmaster@sandbox739836cc694a4f40946a6e325750d279.mailgun.org',
